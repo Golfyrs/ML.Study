52bc023ee7b27d93be0efff96c75c87e5fedf8eb
Golfyrs <golfyrs@gmail.com>
2020-05-13 21:43:19 +0300

    Some minor refactored.

--- IndirectlyRelated/OpenCV/background_reduction/background_reduction.py
+++ IndirectlyRelated/OpenCV/background_reduction/background_reduction.py
@@ -7,11 +7,10 @@ fgbg = cv2.createBackgroundSubtractorMOG2()
 while(1):
     ret, frame = cap.read()
 
-    fgmask = fgbg.apply(frame)
+    FG_mask = fgbg.apply(frame)
  
-    cv2.imshow('fgmask',frame)
-    cv2.imshow('frame',fgmask)
-
+    cv2.imshow('fgmask', frame)
+    cv2.imshow('frame', FG_mask)
     
     k = cv2.waitKey(30) & 0xff
     if k == 27:
